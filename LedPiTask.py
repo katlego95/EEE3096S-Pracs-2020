3956463044773d0fc23335d8aade522b8aa9471c
Katlego Gaveni <katlegogaveni@Katlegos-MacBook-Air.local>
2020-09-26 10:23:38 +0200

    set up push button GPIO

--- LedPiTask.py
+++ LedPiTask.py
@@ -3,6 +3,13 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 LEDPin = 22
+buttonPin = 5
 
 # setup the pin the LED is connected to
 GPIO.setup(LEDPin, GPIO.OUT)
+
+# setup the push button
+GPIO.setup(buttonPin, GPIO.IN,pull_up_down= GPIO.PUD_UP)
+
+buttonState= True
+ledState = False
