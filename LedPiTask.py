2b71695cc6014f82badc52260d504e08e90205d6
Katlego Gaveni <katlegogaveni@Katlegos-MacBook-Air.local>
2020-09-26 10:43:43 +0200

    initial turning of LED on/off

--- LedPiTask.py
+++ LedPiTask.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 GPIO.setmode(GPIO.BCM)
 
 LEDPin = 22
@@ -7,9 +7,21 @@ buttonPin = 5
 
 # setup the pin the LED is connected to
 GPIO.setup(LEDPin, GPIO.OUT)
-
 # setup the push button
 GPIO.setup(buttonPin, GPIO.IN,pull_up_down= GPIO.PUD_UP)
 
-buttonState= True
-ledState = False
+#initial state of LED and push button
+LEDState = False
+ButtonState = True
+
+#turning LED on and Off
+LEDState = True
+GPIO.output(LEDPin, LEDState)
+time.sleep(2)
+LEDState = False
+GPIO.output(LEDPin,LEDState)
+
+
+
+
+
