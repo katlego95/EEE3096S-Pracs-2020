b5ff3d08eaa7502f7efe73919069921f1a143c52
Katlego Gaveni <katlegogaveni@Katlegos-MacBook-Air.local>
2020-09-26 11:14:50 +0200

    set up while loop for toggling LED

--- LedPiTask.py
+++ LedPiTask.py
@@ -1,27 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 
+#GPIO pins
 LEDPin = 22
 buttonPin = 5
 
 # setup the pin the LED is connected to
 GPIO.setup(LEDPin, GPIO.OUT)
-# setup the push button
+# setup the push button pin
 GPIO.setup(buttonPin, GPIO.IN,pull_up_down= GPIO.PUD_UP)
+#LED defualt state
+GPIO.output(LEDPin,False)
 
-#initial state of LED and push button
-LEDState = False
-ButtonState = True
-
-#turning LED on and Off
-LEDState = True
-GPIO.output(LEDPin, LEDState)
-time.sleep(2)
-LEDState = False
-GPIO.output(LEDPin,LEDState)
-
-
-
-
-
+try:
+	#turning LED on and Off
+	GPIO.output(LEDPin,True)
+	print ("LED ON")
+	time.sleep(2)
+	GPIO.output(LEDPin,False)
+	Print ("LED OFF")
+        
+	print ("Ready you can press the button")
+	while True:
+		#Push button toggles LED 
+		GPIO.output(LEDPin, not GPIO.input(buttonPin))
+		time.sleep(.1)
+		
+finally:
+	#Reset the GPIO pins to a safe state
+	GPIO.output(LEDPin,False)
+	GPIO.cleanup()
